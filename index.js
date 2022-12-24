893c780e1f11af63f0f34ceadc2ca698a23fd9d1
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-12-24 16:23:05 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -2,21 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-
-const newsCard = require('./data/NewsCard.json')
+const newsCard = require("./data/NewsCard.json");
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const uri = `mongodb+srv://${process.env.BD_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.4lwt8qz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.get('/newsCard', (req, res) => {
-    res.send(newsCard)
-})
+async function run() {
+  try {
+    app.get("/", (req, res) => {
+      res.send("Fancy nomad api is running...");
+    });
 
-
+    app.get("/newsCard", (req, res) => {
+      res.send(newsCard);
+    });
+  } finally {
+  }
+}
+run().catch(console.log(err.message));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
